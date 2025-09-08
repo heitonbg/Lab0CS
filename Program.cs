c5ca891ae740b89530f4c8ad9654d15faea84acc
iwrawww@gmail.com <iwrawww@gmail.com>
2025-09-08 15:07:49 +0700

    сделал 2 задание(у меня телефонный справочник)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace lab1
 {
+    public class Subscriber
+    {
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string InstallYear { get; set; }
+    }
     class Program
     {
         static void firstTask()
@@ -20,12 +29,54 @@ namespace lab1
 
         static void secondTask()
         {
+            var subscribers = new List<Subscriber>
+            {
+                new Subscriber { LastName = "Иванов", PhoneNumber = "1277", InstallYear = "2015" },
+                new Subscriber { LastName = "Петров", PhoneNumber = "2345", InstallYear = "1999" },
+                new Subscriber { LastName = "Сидоров", PhoneNumber = "4577", InstallYear = "1977" },
+                new Subscriber { LastName = "Кузнецов", PhoneNumber = "7811", InstallYear = "2011" },
+                new Subscriber { LastName = "Смирнов", PhoneNumber = "3399", InstallYear = "2020" },
+                new Subscriber { LastName = "Попов", PhoneNumber = "6688", InstallYear = "1988" },
+                new Subscriber { LastName = "Васильев", PhoneNumber = "1233", InstallYear = "2023" }
+            };
 
+            string json = System.Text.Json.JsonSerializer.Serialize(subscribers, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("phone_directory.json", json);
+            Console.WriteLine("Данные сохранены в файл 'phone_directory.json'");
+
+            string jsonData = File.ReadAllText("phone_directory.json");
+            List<Subscriber> data = System.Text.Json.JsonSerializer.Deserialize<List<Subscriber>>(jsonData);
+
+            var filteredSubscribers = data.FindAll(s =>
+                s.PhoneNumber.Length == 4 &&
+                s.InstallYear.Length == 4 &&
+                s.PhoneNumber.Substring(2, 2) == s.InstallYear.Substring(2, 2));
+
+            Console.WriteLine("\nВсе абоненты:");
+            foreach (var sub in data)
+            {
+                Console.WriteLine($"{sub.LastName,-10} {sub.PhoneNumber} ({sub.InstallYear})");
+            }
+
+            Console.WriteLine("\nАбоненты, у которых yy=бб: ");
+            if (filteredSubscribers.Count > 0)
+            {
+                foreach (var sub in filteredSubscribers)
+                {
+                    string yy = sub.PhoneNumber.Substring(2, 2);
+                    string bb = sub.InstallYear.Substring(2, 2);
+                    Console.WriteLine($"{sub.LastName,-10} {sub.PhoneNumber} ({sub.InstallYear}) - yy={yy}, бб={bb}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Абонентов, удовлетворяющих условию, не найдено.");
+            }
         }
 
         static void Main()
         {
-            Console.WriteLine("1. Возведение в степень.\n2. Переместить вторую цифру в конец.\n");
+            Console.WriteLine("Фенько Андрей - Вариант 8\n1. Решение задачи про плотность.\n2. Задача про телефонный справочник.\n");
 
             Console.Write("Введите выбранный пункт: ");
             string UserAnswer = Console.ReadLine();
@@ -39,7 +90,7 @@ namespace lab1
                     secondTask();
                     break;
                 default:
-                    Console.WriteLine("\nВыбранного пункта не существует! Повторите попытку!\n");
+                    Console.WriteLine("\nВыбранного пункта не существует!\n");
                     break;
             }
         }
